aeda620081254daec92265ab71230f5363275fc0
abhijeetrocks111 <abhijeet@elsner.in>
2021-09-21 12:01:53 +0530

    Update chromedriverlocation

--- src/test/java/baseTestScripts/TestData.java
+++ src/test/java/baseTestScripts/TestData.java
@@ -6,7 +6,7 @@ public class TestData {
 //	public static final String WMS_url = "https://wms.php.fmv.cc/signin";
 	public static final String WMS_url = "https://wmstest.php.fmv.cc/signin";
 	public static final String WMS_HomepageTitle = "WMS- Admin Dashboard";
-	public static final String driver_location = "C:\\Users\\Lenovo\\Documents\\GitHub\\wms\\resources\\chromedriver.exe";
+	public static final String driver_location = "C:\\Users\\Lenovo\\git\\wms_trunk\\resources\\chromedriver.exe";
 	public static final String email = "abhijeet@elsner.in";
 	public static final String password = "Test@123";
 	public static final String user_fullname = "Automation User";
